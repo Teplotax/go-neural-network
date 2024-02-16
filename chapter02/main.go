455601f6fa1dbbf74bbb54768f12e76bac153f56
Teplotax <eq.tfreis@gmail.com>
2024-02-16 02:04:34 -0300

    Working with input batches

--- chapter02/main.go
+++ chapter02/main.go
@@ -11,26 +11,59 @@ func main() {
 	start := time.Now()
 	defer microsecondsSince(start)
 
-	inputs := t.New(t.WithBacking([]float32{1, 2, 3, 2.5}))
+	// var inputs t.Tensor = t.New(t.WithShape(1, 4), t.WithBacking([]float32{1, 2, 3, 2.5}))
+	// bias := t.New(t.WithBacking([]float32{2, 3, 0.5}))
 
+	// rawWeights := []float32{
+	// 	0.2, 0.8, -0.5, 1.0,
+	// 	0.5, -0.91, 0.26, -0.5,
+	// 	-0.26, -0.27, 0.17, 0.87,
+	// }
+
+	// weights := t.New(t.WithShape(bias.Size(), inputs.Size()), t.WithBacking(rawWeights))
+
+	// dotProduct, _ := t.Dot(weights, inputs)
+	// neuronOutput, _ := t.Add(dotProduct, bias)
+
+	// fmt.Println(neuronOutput)
+	rawInputs := []float32{
+		1.0, 2.0, 3.0, 2.5,
+		2.0, 5.0, -1, 2.0,
+		-1.5, 2.7, 3.3, -0.8,
+	}
 	rawWeights := []float32{
 		0.2, 0.8, -0.5, 1.0,
 		0.5, -0.91, 0.26, -0.5,
 		-0.26, -0.27, 0.17, 0.87,
 	}
+	rawBiases := []float32{2.0, 3.0, 0.5}
 
+	inputs := t.New(t.WithShape(3, 4), t.WithBacking(rawInputs))
 	weights := t.New(t.WithShape(3, 4), t.WithBacking(rawWeights))
+	biases := biasTensor(rawBiases, inputs.Shape()[0])
+	weights.T()
 
-	bias := t.New(t.WithBacking([]float32{2, 3, 0.5}))
+	dP, _ := t.Dot(inputs, weights)
 
-	dotProduct, _ := t.Dot(weights, inputs)
-	neuronOutput, _ := t.Add(dotProduct, bias)
+	outputs, err := t.Add(dP, biases)
+	if err != nil {
+		fmt.Println(err)
+	}
 
-	fmt.Println(neuronOutput)
-
+	fmt.Println(outputs)
 }
 
 func microsecondsSince(start time.Time) {
 	duration := time.Since(start)
 	fmt.Println("Execution time: ", duration.Microseconds(), "μs")
+}
+
+func biasTensor(rawBiases []float32, rows int) t.Tensor {
+	newRawBiases := []float32{}
+
+	for i := 0; i < rows; i++ {
+		newRawBiases = append(newRawBiases, rawBiases...)
+	}
+
+	return t.New(t.WithShape(rows, len(rawBiases)), t.WithBacking(newRawBiases))
 }

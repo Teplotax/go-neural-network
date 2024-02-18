f1053bee03214431f17b941f1ee4d354b7ea1f6a
Teplotax <eq.tfreis@gmail.com>
2024-02-18 01:24:06 -0300

    Minor refactoring

--- chapter02/main.go
+++ chapter02/main.go
@@ -9,23 +9,8 @@ import (
 
 func main() {
 	start := time.Now()
-	defer microsecondsSince(start)
+	defer MicrosecondsSince(start)
 
-	// var inputs t.Tensor = t.New(t.WithShape(1, 4), t.WithBacking([]float32{1, 2, 3, 2.5}))
-	// bias := t.New(t.WithBacking([]float32{2, 3, 0.5}))
-
-	// rawWeights := []float32{
-	// 	0.2, 0.8, -0.5, 1.0,
-	// 	0.5, -0.91, 0.26, -0.5,
-	// 	-0.26, -0.27, 0.17, 0.87,
-	// }
-
-	// weights := t.New(t.WithShape(bias.Size(), inputs.Size()), t.WithBacking(rawWeights))
-
-	// dotProduct, _ := t.Dot(weights, inputs)
-	// neuronOutput, _ := t.Add(dotProduct, bias)
-
-	// fmt.Println(neuronOutput)
 	rawInputs := []float32{
 		1.0, 2.0, 3.0, 2.5,
 		2.0, 5.0, -1, 2.0,
@@ -40,30 +25,14 @@ func main() {
 
 	inputs := t.New(t.WithShape(3, 4), t.WithBacking(rawInputs))
 	weights := t.New(t.WithShape(3, 4), t.WithBacking(rawWeights))
-	biases := biasTensor(rawBiases, inputs.Shape()[0])
 	weights.T()
 
 	dP, _ := t.Dot(inputs, weights)
 
-	outputs, err := t.Add(dP, biases)
+	outputs, err := LayerOutput(dP, rawBiases)
 	if err != nil {
 		fmt.Println(err)
 	}
 
 	fmt.Println(outputs)
-}
-
-func microsecondsSince(start time.Time) {
-	duration := time.Since(start)
-	fmt.Println("Execution time: ", duration.Microseconds(), "μs")
-}
-
-func biasTensor(rawBiases []float32, rows int) t.Tensor {
-	newRawBiases := []float32{}
-
-	for i := 0; i < rows; i++ {
-		newRawBiases = append(newRawBiases, rawBiases...)
-	}
-
-	return t.New(t.WithShape(rows, len(rawBiases)), t.WithBacking(newRawBiases))
 }
---
+++ chapter02/util_funcs.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	t "gorgonia.org/tensor"
+)
+
+func LayerOutput(dotProduct t.Tensor, rawBiases []float32) (t.Tensor, error) {
+
+	newShape := dotProduct.Shape()
+	newRawBiases := []float32{}
+
+	for i := 0; i < newShape[0]; i++ {
+		newRawBiases = append(newRawBiases, rawBiases...)
+	}
+
+	biases := t.New(t.WithShape(newShape...), t.WithBacking(newRawBiases))
+
+	return t.Add(dotProduct, biases)
+}
+
+func MicrosecondsSince(start time.Time) {
+	duration := time.Since(start)
+	fmt.Println("Execution time: ", duration.Microseconds(), "μs")
+}

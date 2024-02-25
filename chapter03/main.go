d693951eb5b7248eea1b41e86362985616c0cd56
Teplotax <eq.tfreis@gmail.com>
2024-02-25 11:52:03 -0300

    using tensor package softmax activation func

--- chapter03/main.go
+++ chapter03/main.go
@@ -2,62 +2,71 @@ package main
 
 import (
 	"fmt"
-	"math"
 	"time"
 
 	timing "github.com/Teplotax/go-neural-network/Timing"
+	"github.com/Teplotax/go-neural-network/ml"
 )
 
 func main() {
 	start := time.Now()
 	defer timing.MicrosecondsSince(start)
 
-	// make the layer
+	// make first layer
+	dense1 := ml.NewLayerDense(2, 3)
+	activation1 := ml.NewActivationReLU()
 
-	// dense1 := ml.NewLayerDense(2, 3)
-	// activation1 := ml.NewActivationReLU()
+	// make second layer
+	dense2 := ml.NewLayerDense(3, 3)
+	activation2 := ml.NewActivationSoftMax()
 
 	// forward pass
-	// dense1.Forward(X)
-	// activation1.Forward(dense1.Outputs)
+	dense1.Forward(X)
+	activation1.Forward(dense1.Outputs)
+
+	dense2.Forward(dense1.Outputs)
+	activation2.Forward(dense2.Outputs, 1)
+
+	fmt.Println(activation2.Outputs)
 
 	// fmt.Println(activation1.Outputs)
 
 	// p.PlotData(X, Y, CMap, "plot.png")
 
 	//---------------------------------------
+	// SoftMax activation function
+	// outputs := [][]float64{
+	// 	{4.8, 1.21, 2.385},
+	// 	{8.9, -1.81, 0.2},
+	// 	{1.41, 1.051, 0.026},
+	// }
 
-	outputs := [][]float64{
-		{4.8, 1.21, 2.385},
-		{8.9, -1.81, 0.2},
-		{1.41, 1.051, 0.026},
-	}
+	// expVals := make([][]float64, len(outputs))
+	// normVals := make([][]float64, len(expVals))
 
-	expVals := make([][]float64, len(outputs))
-	normVals := make([][]float64, len(expVals))
+	// for i, output := range outputs {
 
-	for i, output := range outputs {
+	// 	var normBase float64 = 0
+	// 	var max float64 = output[0]
+	// 	expVals[i] = make([]float64, len(output))
+	// 	normVals[i] = make([]float64, len(output))
 
-		var normBase float64 = 0
-		var max float64 = output[0]
-		expVals[i] = make([]float64, len(output))
-		normVals[i] = make([]float64, len(output))
+	// 	for _, val := range output {
+	// 		max = math.Max(max, val)
+	// 	}
 
-		for _, val := range output {
-			max = math.Max(max, val)
-		}
+	// 	for j, val := range output {
+	// 		expVals[i][j] = math.Pow(math.E, val-max)
+	// 		max = math.Max(max, expVals[i][j])
+	// 		normBase += expVals[i][j]
+	// 	}
 
-		for j, val := range output {
-			expVals[i][j] = math.Pow(math.E, val-max)
-			max = math.Max(max, expVals[i][j])
-			normBase += expVals[i][j]
-		}
+	// 	for l, val := range expVals[i] {
+	// 		normVals[i][l] = val / normBase
+	// 	}
+	// }
 
-		for l, val := range expVals[i] {
-			normVals[i][l] = val / normBase
-		}
-	}
-
-	fmt.Println(normVals)
+	// fmt.Println(normVals)
+	//---------------------------------------
 
 }
